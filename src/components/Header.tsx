eecc4455849bb4c24c78d0224a65146d12577b24
Viole18 <violejgrays@gmail.com>
2024-02-04 19:26:52 +0300

    12

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src="https://cdn.worldvectorlogo.com/logos/png-1.svg" alt="logo" />
+      </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,23 @@
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+        <img src="https://klike.net/uploads/posts/2022-10/1664882480_b.jpg" alt="fone" />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          new post
+        </div>
+        <div>
+          <div>Post1</div>
+          <div>Post2</div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile
