80e4ca0349f4e986e7b3b016ee829bd8acf99688
Viole18 <violejgrays@gmail.com>
2024-02-08 00:15:11 +0300

    22

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,37 @@
+import { NavLink } from 'react-router-dom'
 import classes from './Dialogs.module.css'
 
 const Dialogs = (props: any) => {
     return (
         <div>
-            Dialogs
+            <div className={classes.dialogs}>
+                <div className={classes.dialogsItems}>
+                    <div className={classes.dialog + ' ' + classes.active}>
+                        <NavLink to='/dialogs/1'>Dimych</NavLink>
+                    </div>
+                    <div className={classes.dialog}>
+                        <NavLink to='/dialogs/2'>Artem</NavLink>
+                    </div>
+                    <div className={classes.dialog}>
+                        <NavLink to='/dialogs/3'>Sveta</NavLink>
+                    </div>
+                    <div className={classes.dialog}>
+                        <NavLink to='/dialogs/4'>Valera</NavLink>
+                    </div>
+                    <div className={classes.dialog}>
+                        <NavLink to='/dialogs/5'>Slava</NavLink>
+                    </div>
+                    <div className={classes.dialog}>
+                        <NavLink to='/dialogs/6'>Lesha</NavLink>
+                    </div>
+                </div>
+                <div className={classes.messages}>
+                    <div className={classes.message}>Hi</div>
+                    <div className={classes.message}>How are you?</div>
+                    <div className={classes.message}>Yo</div>
+                </div>
+            </div>
+
         </div>
     )
 }
