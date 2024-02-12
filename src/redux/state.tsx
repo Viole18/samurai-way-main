7493c7e5be33d8280028195812e97c0fdab5c7aa
Viole18 <violejgrays@gmail.com>
2024-02-12 20:40:05 +0300

    29

---
+++ src/redux/state.tsx
@@ -0,0 +1,31 @@
+let state = {
+    profilePage: {
+        posts: [
+        { id: 1, message: 'Hi, how are you?', likesCount: 0 },
+        { id: 2, message: 'How are you?', likesCount: 18 },
+        { id: 3, message: 'Hi, you?', likesCount: 32 },
+        { id: 4, message: 'Da DA DADADADAD?', likesCount: 12 },
+    ],
+    },
+    dialogsPage: {
+        dialogs: [
+        { id: 1, name: 'Dimych' },
+        { id: 2, name: 'Artem' },
+        { id: 3, name: 'Sveta' },
+        { id: 4, name: 'Valera' },
+        { id: 5, name: 'Slava' },
+        { id: 6, name: 'Lesha' },
+    ],
+    messages: [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'How are you?' },
+        { id: 3, message: 'Yo' },
+        { id: 4, message: 'Yo' },
+        { id: 5, message: 'Yo' },
+        { id: 6, message: 'Yo' },
+    ]
+    }
+    
+}
+
+export default state
